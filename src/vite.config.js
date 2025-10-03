7f5241a53cfd4ff23b5180f3cddea82a7a44ea85
Jorge Cedeño <jcedenotn2504@gmail.com>
2025-10-03 15:34:46 -0500

    vite-config-update

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,8 +1,8 @@
 
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 
 export default defineConfig({
-    base: '/js-21-JorgeCede-o/'
+    base: '/js-21-JorgeCede-o/',
 
-})
+});
